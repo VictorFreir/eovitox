d530b7ac35eace51a90f7d2300fb10f1c90b0bd6
VictorFreir <victorvfreire@gmail.com>
2023-11-14 16:59:15 -0300

    Add balloon animation

--- script.js
+++ script.js
@@ -4,20 +4,54 @@ async function animateMovement() {
     var viewportWidth = window.innerWidth;
     var topValueInVw = (parseFloat(computedStyle.top) / viewportWidth) * 100;
 
-    console.log(topValueInVw + "vw");
-
     var maxValue = -100;
-
+    let speed = 0.8
     if (topValueInVw > maxValue) {
-        centralButtonsSection.style.top = topValueInVw - 0.5 + "vw";
-
+        centralButtonsSection.style.top = topValueInVw - speed + "vw";
+        console.log(topValueInVw);
         await new Promise(requestAnimationFrame);
 
         await animateMovement();
     }
 }
 
-function pressed_button() {
-    animateMovement();
-    console.log(document.getElementsByName("ballon"));
+async function disapear_balloon(){
+    var balloon = document.getElementsByName("ballon")[0];
+
+    var current_opacity = getComputedStyle(balloon).opacity;
+    current_opacity = parseFloat(current_opacity)*100; 
+
+    let speed = 3;
+
+    if (current_opacity >= 0){
+        balloon.style.setProperty("opacity", current_opacity-speed+"%");
+        console.log(current_opacity);
+        await new Promise(requestAnimationFrame);
+
+        await disapear_balloon()
+    }
+
+}
+
+async function run_person(){
+    let person = document.getElementById("person");
+    
+    let person_size = parseFloat(getComputedStyle(person).width);
+    let maxValue = window.innerWidth;   
+    let positionX = parseFloat(getComputedStyle(person).left);
+    
+    let speed = 5;
+    if (positionX <= maxValue){
+        console.log(positionX)
+        person.style.setProperty("left", positionX+speed+"px");
+        await new Promise(requestAnimationFrame);
+        await run_person();
+    }
+    
+
+}
+
+async function pressed_button() {
+    await Promise.all([disapear_balloon(), animateMovement()]);
+
 }
